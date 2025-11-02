71e0b3b9ffb9aa6c231fa90f45fa3493cf0ed798
Rimsky1337 <39468231+Rimsky1337@users.noreply.github.com>
2025-11-03 02:27:47 +0300

    Update configure_zaprets.sh

--- configure_zaprets.sh
+++ configure_zaprets.sh
@@ -204,65 +204,6 @@ then
 fi
 
 echo "Configure dhcp..."
-
-uci set dhcp.cfg01411c.strictorder='1'
-uci set dhcp.cfg01411c.filter_aaaa='1'
-uci add_list dhcp.cfg01411c.server='127.0.0.1#5053'
-uci add_list dhcp.cfg01411c.server='127.0.0.1#5054'
-uci add_list dhcp.cfg01411c.server='127.0.0.1#5055'
-uci add_list dhcp.cfg01411c.server='127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.chatgpt.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.oaistatic.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.oaiusercontent.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.openai.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.microsoft.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.windowsupdate.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.bing.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.supercell.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.seeurlpcl.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.supercellid.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.supercellgames.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.clashroyale.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.brawlstars.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.clash.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.clashofclans.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.x.ai/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.grok.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.github.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.forzamotorsport.net/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.forzaracingchampionship.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.forzarc.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.gamepass.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.orithegame.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.renovacionxboxlive.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.tellmewhygame.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xbox.co/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xbox.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xbox.eu/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xbox.org/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xbox360.co/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xbox360.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xbox360.eu/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xbox360.org/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xboxab.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xboxgamepass.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xboxgamestudios.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xboxlive.cn/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xboxlive.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xboxone.co/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xboxone.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xboxone.eu/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xboxplayanywhere.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xboxservices.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xboxstudios.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.xbx.lv/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.sentry.io/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.usercentrics.eu/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.recaptcha.net/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.gstatic.com/127.0.0.1#5056'
-uci add_list dhcp.cfg01411c.server='/*.brawlstarsgame.com/127.0.0.1#5056'
-uci commit dhcp
-
 echo "Add unblock ChatGPT..."
 
 checkAndAddDomainPermanentName "chatgpt.com" "83.220.169.155"
@@ -276,23 +217,6 @@ str=$(grep -i "$nameRule" /etc/config/firewall)
 if [ -z "$str" ] 
 then
   echo "Add block QUIC..."
-
-  uci add firewall rule # =cfg2492bd
-  uci set firewall.@rule[-1].name='Block_UDP_80'
-  uci add_list firewall.@rule[-1].proto='udp'
-  uci set firewall.@rule[-1].src='lan'
-  uci set firewall.@rule[-1].dest='wan'
-  uci set firewall.@rule[-1].dest_port='80'
-  uci set firewall.@rule[-1].target='REJECT'
-  uci add firewall rule # =cfg2592bd
-  uci set firewall.@rule[-1].name='Block_UDP_443'
-  uci add_list firewall.@rule[-1].proto='udp'
-  uci set firewall.@rule[-1].src='lan'
-  uci set firewall.@rule[-1].dest='wan'
-  uci set firewall.@rule[-1].dest_port='443'
-  uci set firewall.@rule[-1].target='REJECT'
-  uci commit firewall
-  service firewall restart
 fi
 
 cronTask="0 4 * * * wget -O - $URL/configure_zaprets.sh | sh"
